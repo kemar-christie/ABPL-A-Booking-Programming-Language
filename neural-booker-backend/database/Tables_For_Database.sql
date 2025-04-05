c1f72a42733c8f6f333070a19adf8280be88113b
Kemar Christie <kemar.christie@yahoo.com>
2025-04-05 15:02:47 -0500

    Refactor database schema: rename balance to account_balance, add total_price to Booking_Information, and implement triggers for outstanding balance updates.

--- neural-booker-backend/database/Tables_For_Database.sql
+++ neural-booker-backend/database/Tables_For_Database.sql
@@ -1,7 +1,6 @@
 -- Copyright (c) 2025 Kemar Christie, Roberto James, Dwayne Gibbs, Tyoni Davis, Danielle Jones
 -- Authors: Kemar Christie, Roberto James, Dwayne Gibbs, Tyoni Davis, Danielle Jones
 
--- Query to Create Customer Information Table.
 CREATE TABLE "Customer_Information"
 (
 	customer_id SERIAL PRIMARY KEY,
@@ -9,7 +8,7 @@ CREATE TABLE "Customer_Information"
 	first_name VARCHAR(100) NOT NULL,
 	last_name VARCHAR(100) NOT NULL,
 	email_address VARCHAR(255) UNIQUE NOT NULL,
-	balance NUMERIC(10,2) NOT NULL
+	account_balance NUMERIC(10,2) NOT NULL
 );
 
 -- Query to Create Booking Information Table.
@@ -18,7 +17,9 @@ CREATE TABLE "Booking_Information"
 	booking_id SERIAL PRIMARY KEY,
 	booking_type VARCHAR(50) NOT NULL,
 	date_of_booking TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	booking_status booking_status_enum NOT NULL
+	booking_status booking_status_enum NOT NULL,
+	total_price NUMERIC(10,2) NOT NULL,
+    oustanding_balance NUMERIC(10,2)
 );
 
 -- Query to Create Payment Information Table.
@@ -26,10 +27,8 @@ CREATE TABLE "Payment_Information"
 (
 	payment_id SERIAL PRIMARY KEY,
 	booking_id INT NOT NULL,
-	total_price NUMERIC(10,2) NOT NULL,
-    amount_paid NUMERIC(10,2) NOT NULL,
-    balance NUMERIC(10,2) GENERATED ALWAYS AS (total_price - amount_paid) STORED,
 	payment_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    amount_paid NUMERIC(10,2) NOT NULL,
 	payment_method payment_method_enum NOT NULL,
 	FOREIGN KEY (booking_id) REFERENCES "Booking_Information"(booking_id)
 );
@@ -45,16 +44,17 @@ INSERT INTO "Customer_Information"
 	first_name, 
 	last_name, 
 	email_address, 
-	balance
+	account_balance
 ) 
 VALUES
 ('1', 'Kemar', 'Christie', 'kemarchristie@example.com', 50000),
 ('2', 'Roberto', 'James', 'robertojames@example.com', 50000),
 ('3', 'Dwayne', 'Gibbs', 'dwaynegibbs@example.com', 50000),
 ('4', 'Tyoni', 'Davis', 'tyonidavis@example.com', 50000),
-('5', 'Danielle', 'Jones', 'daniellejones@example.com', 50000);
-('Benjamin', 'Robinson', 'benjaminrobinson@example.com', 50000);
-('Marcus', 'Manley', 'marcusmanley@example.com', 50000);
+('5', 'Danielle', 'Jones', 'daniellejones@example.com', 50000),
+('Benjamin', 'Robinson', 'benjaminrobinson@example.com', 50000),
+('Marcus', 'Manley', 'marcusmanley@example.com', 50000),
+('David', 'White', 'davidwhite@example.com', 50000);
 
 SELECT * FROM "Customer_Information";
 
@@ -70,11 +70,12 @@ SHOW TIMEZONE;
 COMMENT ON COLUMN your_table.column_name IS 'Comment';
 
 -- Query for commenting on tables.
-COMMENT ON TABLE your_table IS 'Comment.';
+COMMENT ON TABLE "your_table" IS 'Comment.';
 
 -- This sets the sequence to match the highest customer_id you've inserted so that future auto-increments will be correct.
-SELECT pg_get_serial_sequence("Customer_Information", 'customer_id');
+SELECT pg_get_serial_sequence('"Customer_Information"', 'customer_id');
 SELECT setval('public.customer_customer_id_seq', (SELECT MAX(customer_id) FROM "Customer_Information"));
+
 
 -- Auto-generate customer_username based on name + ID
 CREATE OR REPLACE FUNCTION generate_customer_username()
@@ -88,16 +89,37 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 --
-CREATE TRIGGER trg_generate_username
+
+CREATE TRIGGER trg_generate_customer_username
 BEFORE INSERT ON "Customer_Information"
 FOR EACH ROW
 EXECUTE FUNCTION generate_customer_username();
 
--- Query to Edit Columns.
+
+-- Update the balance to the outstanding balance after the calculation is done.
+CREATE OR REPLACE FUNCTION update_outstanding_balance()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Calculate the outstanding balance: total_price - amount_paid
+    UPDATE "Booking_Information"
+    SET balance = total_price - NEW.amount_paid
+    WHERE booking_id = NEW.booking_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+--
+
+CREATE TRIGGER trg_update_oustanding_balance
+AFTER INSERT ON "Payment_Information"
+FOR EACH ROW
+EXECUTE FUNCTION update_outstanding_balance();
+
+
+-- Query to Edit Columns datatypes etc.
 ALTER TABLE name_of_table
 ALTER COLUMN name_column DROP NOT NULL;
 
--- Query to Delete Data from the table based on column.
+-- Query to Delete Data from the table based on a column.
 DELETE FROM name_of_table
 WHERE name_of_field = "value";
 
@@ -111,3 +133,16 @@ RENAME TO new_table_name;
 -- Query to Rename a column.
 ALTER TABLE name_of_table
 RENAME COLUMN old_column_name TO new_column_name;
+
+-- Query to Delete a Column.
+ALTER TABLE name_of_table
+DROP COLUMN name_of_column;
+
+-- Query to Delete a Table.
+DROP TABLE name_of_table;
+
+-- Query to Delete Trigger.
+DROP TRIGGER name_of_trigger ON name_of_table;
+
+-- Query to Delete Function.
+DROP FUNCTION name_of_function();
